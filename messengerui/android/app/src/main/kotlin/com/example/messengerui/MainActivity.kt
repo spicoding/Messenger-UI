79f1594245a21837fee037e360cbc611de594098
spicoding <shaneinduli@gmail.com>
2024-05-14 17:29:48 +0300

    initial commit

---
+++ messengerui/android/app/src/main/kotlin/com/example/messengerui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.messengerui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ messengerui/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:messengerui/screens/conversations.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Messenger Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Conversations(),
+    );
+  }
+}
